7893a925d8fa088bd141bbe3b9f3f225faa8dea6
NhattriExpress <95321284+NhattriExpress@users.noreply.github.com>
2021-12-03 19:19:23 +0700

    Create index_private_1.js

---
+++ index_private_1.js
@@ -0,0 +1,17 @@
+//lắng nghe thông tin từ nội dung trong iframe để xử lý url của iframe hay thao tác khác
+window.addEventListener("storage", myFunction);
+
+function myFunction(event) {
+  var str = localStorage.getItem("mytime");
+  localStorage.removeItem("mytime");
+  
+  var arr = str.split("|");
+  if (arr[1] == "c") {
+    document.getElementById("myFrame").src = arr[2];
+  }
+
+}
+
+function changeValue() {
+  localStorage.setItem("mytime", Date.now() + "|" + "p" + "|" + "giatri truyen tai day");
+}
