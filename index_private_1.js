cbfa5e250b3b2ffd5fde259e670915e3e5e9abc7
NhattriExpress <95321284+NhattriExpress@users.noreply.github.com>
2021-12-04 23:32:32 +0700

    Update index_private_1.js

--- index_private_1.js
+++ index_private_1.js
@@ -1,3 +1,27 @@
+var exec = "https://script.google.com/macros/s/AKfycbw8MfEDiLd4GRkWVHDxaiqZlmKSbhDOHpcGX2Xc0FpanBUtxMY/exec";
+
+
+    window.addEventListener("load",function() {
+	    
+        var str = window.location.href;
+	var action = str.split("?")[1];
+	if (action){
+	   str = "?action=" + action;
+	} else {
+	   str = "?embedIframe";
+	}	
+	
+	    
+	    //document.getElementById("myFrame").src = "https://script.google.com/macros/s/AKfycby7xOajlwfyrlDp0vR-3mxXC5O95s5uPbh3cz8C/exec" + str;
+	    var ifr = document.getElementById("myFrame");
+	    //ifr.contentWindow.location.replace("https://script.google.com/macros/s/AKfycbw8MfEDiLd4GRkWVHDxaiqZlmKSbhDOHpcGX2Xc0FpanBUtxMY/exec" + str); 
+	    
+		//alert(document.getElementById("myFrame").localStorage.getItem("QQQ"));
+      
+      //ifr.src = "https://script.google.com/macros/s/AKfycbw8MfEDiLd4GRkWVHDxaiqZlmKSbhDOHpcGX2Xc0FpanBUtxMY/exec" + str;
+      ifr.contentWindow.location.replace("https://script.google.com/macros/s/AKfycbw8MfEDiLd4GRkWVHDxaiqZlmKSbhDOHpcGX2Xc0FpanBUtxMY/exec" + str);
+        });
+
 //lắng nghe thông tin từ nội dung trong iframe để xử lý url của iframe hay thao tác khác
 window.addEventListener("storage", myFunction);
 
